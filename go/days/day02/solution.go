d010fec19df69fc5176ce2a0cd258ca583ea9e76
Lâm Ngyen Hoang <lamnguyenhoang@gmail.com>
2021-12-02 06:53:27 +0100

    Day 2

--- go/days/day02/solution.go
+++ go/days/day02/solution.go
@@ -3,22 +3,61 @@ package day02
 import (
 	"fmt"
 	"github.com/lnguyenh/aoc-2021/utils"
+	"strconv"
 )
 
-func Part1(values []int) int {
-	fmt.Println(values)
-	return 0
+func applyOneInstruction(start []int, instruction []string) []int {
+	moveValue, _ := strconv.Atoi(instruction[1])
+	var stop []int
+	switch instruction[0] {
+	case "forward":
+		stop = []int{start[0] + moveValue, start[1]}
+	case "down":
+		stop = []int{start[0], start[1] - moveValue}
+	case "up":
+		stop = []int{start[0], start[1] + moveValue}
+	}
+	return stop
 }
 
-func Part2(values []int) int {
-	fmt.Println(values)
-	return 0
+func applyOneAimInstruction(start []int, aim int, instruction []string) ([]int, int) {
+	changeValue, _ := strconv.Atoi(instruction[1])
+	var stop []int = start
+	var newAim int = aim
+	switch instruction[0] {
+	case "forward":
+		stop = []int{start[0] + changeValue, start[1] + changeValue*aim}
+	case "down":
+		newAim += changeValue
+	case "up":
+		newAim -= changeValue
+	}
+	return stop, newAim
+}
+
+func doPart1(instructions [][]string) int {
+	position := []int{0, 0}
+	for _, instruction := range instructions {
+		position = applyOneInstruction(position, instruction)
+	}
+	result := position[0] * -position[1]
+	return result
+}
+
+func doPart2(instructions [][]string) int {
+	position := []int{0, 0}
+	aim := 0
+	for _, instruction := range instructions {
+		position, aim = applyOneAimInstruction(position, aim, instruction)
+	}
+	result := position[0] * position[1]
+	return result
 }
 
 func Run(path string) {
-	values := utils.ReadFileAsIntArray(path)
-	answer1 := Part1(values)
-	answer2 := Part2(values)
+	values := utils.ReadFileAsArrayOfStringArrays(path)
+	answer1 := doPart1(values)
+	answer2 := doPart2(values)
 	fmt.Printf("Part 1 answer: %v\n", answer1)
 	fmt.Printf("Part 2 answer: %v\n", answer2)
 }
--- go/utils/input_reader.go
+++ go/utils/input_reader.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func ReadFileAsIntArray(path string) []int {
@@ -14,6 +15,18 @@ func ReadFileAsIntArray(path string) []int {
 	for scanner.Scan() {
 		value, _ := strconv.Atoi(scanner.Text())
 		values = append(values, value)
+	}
+	_ = file.Close()
+	return values
+}
+
+func ReadFileAsArrayOfStringArrays(path string) [][]string {
+	var values [][]string
+	file, _ := os.Open(path)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		values = append(values, strings.Split(scanner.Text(), " "))
 	}
 	_ = file.Close()
 	return values
