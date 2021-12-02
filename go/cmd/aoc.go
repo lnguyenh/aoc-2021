59f062fe0c50cd89d8230826a50048965ade56ab
Lâm Ngyen Hoang <lamnguyenhoang@gmail.com>
2021-12-02 08:23:32 +0100

    Day 2: Add convenience to run today as the default problem day

--- go/cmd/aoc.go
+++ go/cmd/aoc.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 	"github.com/lnguyenh/aoc-2021/days"
 	"os"
+	"time"
 )
 
 func main() {
+	var day, inputFilePrefix string
 
 	args := os.Args[1:]
-	day := args[0]
-	input := args[1]
-	inputPath := fmt.Sprintf("../inputs/%s/%s.txt", day, input)
+
+	inputFilePrefix = args[0]
+	if len(args) > 1 {
+		day = args[1]
+	} else {
+		// Use today's date to choose which problem to solve
+		day = fmt.Sprintf("%02d", time.Now().Day())
+	}
+
+	inputPath := fmt.Sprintf("../inputs/%s/%s.txt", day, inputFilePrefix)
 	fmt.Printf("Running AOC for day %s with input '%s'\n", day, inputPath)
 	days.Run(day, inputPath)
 }
