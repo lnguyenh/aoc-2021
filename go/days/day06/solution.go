53294a0731de877ede4a37f97f7aac0e832a788c
Lâm Ngyen Hoang <lamnguyenhoang@gmail.com>
2021-12-06 19:57:25 +0100

    Day 6: cleanup to only keep the optimized solution

--- go/days/day06/solution.go
+++ go/days/day06/solution.go
@@ -5,30 +5,6 @@ import (
 	"github.com/lnguyenh/aoc-2021/utils"
 )
 
-var normalDelay = 6
-var firstDelay = 8
-
-func runOneDay(day []int) []int {
-	newDay := make([]int, 0, len(day))
-	for _, counter := range day {
-		if counter == 0 {
-			newDay = append(newDay, normalDelay)
-			newDay = append(newDay, firstDelay)
-		} else {
-			newDay = append(newDay, counter-1)
-		}
-	}
-	return newDay
-}
-
-func doPart1(initialDay []int) int {
-	day := initialDay
-	for i := 0; i < 80; i++ {
-		day = runOneDay(day)
-	}
-	return len(day)
-}
-
 func initializeCounterMap(day []int) map[int]int {
 	counters := make(map[int]int)
 	for _, counter := range day {
@@ -37,23 +13,31 @@ func initializeCounterMap(day []int) map[int]int {
 	return counters
 }
 
-func runOneMapDay(counters map[int]int) map[int]int {
-	newCounters := make(map[int]int)
-	for daysLeft, numFish := range counters {
-		if daysLeft == 0 {
-			newCounters[normalDelay] += numFish
-			newCounters[firstDelay] = numFish
-		} else {
-			newCounters[daysLeft-1] += numFish
-		}
+func runOneMapDayOptimized(counters map[int]int) {
+	numZeroes := counters[0]
+	counters[0] = counters[1]
+	counters[1] = counters[2]
+	counters[2] = counters[3]
+	counters[3] = counters[4]
+	counters[4] = counters[5]
+	counters[5] = counters[6]
+	counters[6] = counters[7] + numZeroes
+	counters[7] = counters[8]
+	counters[8] = numZeroes
+}
+
+func doPart1(initialDay []int) int {
+	counters := initializeCounterMap(initialDay)
+	for i := 0; i < 80; i++ {
+		runOneMapDayOptimized(counters)
 	}
-	return newCounters
+	return utils.SumIntMap(counters)
 }
 
 func doPart2(initialDay []int) int {
 	counters := initializeCounterMap(initialDay)
 	for i := 0; i < 256; i++ {
-		counters = runOneMapDay(counters)
+		runOneMapDayOptimized(counters)
 	}
 	return utils.SumIntMap(counters)
 }
