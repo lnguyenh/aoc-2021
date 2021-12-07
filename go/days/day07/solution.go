073f9271f1969bcda0ed8773b4c12fff5dd18c79
Lâm Ngyen Hoang <lamnguyenhoang@gmail.com>
2021-12-07 08:13:38 +0100

    Day 7: part 1

--- go/days/day07/solution.go
+++ go/days/day07/solution.go
@@ -5,8 +5,36 @@ import (
 	"github.com/lnguyenh/aoc-2021/utils"
 )
 
-func doPart1() int {
-	return 0
+func getCrabsPerPosition(positions []int) []int {
+	maxX := utils.MaxSlice(positions)
+	crabsPerPositionArray := make([]int, maxX+1)
+	for _, position := range positions {
+		crabsPerPositionArray[position] += 1
+	}
+	return crabsPerPositionArray
+}
+
+func getNumMoves(crabsPerPosition []int, targetPosition int) int {
+	totalMoves := 0
+	for position, numCrabs := range crabsPerPosition {
+		numMoves := numCrabs * utils.IntAbs(targetPosition-position)
+		totalMoves += numMoves
+	}
+	return totalMoves
+}
+
+func doPart1(crabsPerPosition []int) int {
+	// bestPosition := 0
+	minMoves := -1
+	for targetPosition := range crabsPerPosition {
+		numMoves := getNumMoves(crabsPerPosition, targetPosition)
+		fmt.Printf("target position %v num moves %v\n", targetPosition, numMoves)
+		if numMoves < minMoves || minMoves < 0 {
+			// bestPosition = targetPosition
+			minMoves = numMoves
+		}
+	}
+	return minMoves
 }
 
 func doPart2() int {
@@ -14,9 +42,10 @@ func doPart2() int {
 }
 
 func Run(path string) {
-	input := utils.ReadFileAsStringSlice(path, "\n")
-	fmt.Printf("input: %v\n", input)
-	answer1 := doPart1()
+	initialPositions := utils.ParseStringAsIntList(utils.ReadFileAsString(path), ",")
+	crabsPerPosition := getCrabsPerPosition(initialPositions)
+	fmt.Printf("input: %v\n", initialPositions)
+	answer1 := doPart1(crabsPerPosition)
 	answer2 := doPart2()
 	fmt.Printf("Part 1 answer: %v\n", answer1)
 	fmt.Printf("Part 2 answer: %v\n", answer2)
--- go/utils/slices.go
+++ go/utils/slices.go
@@ -8,6 +8,16 @@ func SumSlice(measurement []int) int {
 	return sum
 }
 
+func MaxSlice(slice []int) int {
+	max := 0
+	for _, value := range slice {
+		if value > max {
+			max = value
+		}
+	}
+	return max
+}
+
 func IntInSlice(a int, list []int) bool {
 	for _, b := range list {
 		if b == a {
