6cde8f9421b61db2ae5a34f767d6009c43b5eab2
Lâm Ngyen Hoang <lamnguyenhoang@gmail.com>
2021-12-07 08:37:10 +0100

    Day 7: part 2

--- go/days/day07/solution.go
+++ go/days/day07/solution.go
@@ -14,7 +14,7 @@ func getCrabsPerPosition(positions []int) []int {
 	return crabsPerPositionArray
 }
 
-func getNumMoves(crabsPerPosition []int, targetPosition int) int {
+func getFuelPart1(crabsPerPosition []int, targetPosition int) int {
 	totalMoves := 0
 	for position, numCrabs := range crabsPerPosition {
 		numMoves := numCrabs * utils.IntAbs(targetPosition-position)
@@ -23,30 +23,53 @@ func getNumMoves(crabsPerPosition []int, targetPosition int) int {
 	return totalMoves
 }
 
+func getCostsPart2(crabsPerPosition []int) []int {
+	costs := make([]int, len(crabsPerPosition)*utils.MaxSlice(crabsPerPosition))
+	accumulator := 0
+	for numSteps := range costs {
+		costs[numSteps] = numSteps + accumulator
+		accumulator += numSteps
+	}
+	return costs
+}
+
+func getFuelPart2(crabsPerPosition []int, targetPosition int, costs []int) int {
+	totalFuel := 0
+	for position, numCrabs := range crabsPerPosition {
+		numMoves := utils.IntAbs(targetPosition - position)
+		totalFuel += costs[numMoves] * numCrabs
+	}
+	return totalFuel
+}
+
 func doPart1(crabsPerPosition []int) int {
-	// bestPosition := 0
 	minMoves := -1
 	for targetPosition := range crabsPerPosition {
-		numMoves := getNumMoves(crabsPerPosition, targetPosition)
-		fmt.Printf("target position %v num moves %v\n", targetPosition, numMoves)
+		numMoves := getFuelPart1(crabsPerPosition, targetPosition)
 		if numMoves < minMoves || minMoves < 0 {
-			// bestPosition = targetPosition
 			minMoves = numMoves
 		}
 	}
 	return minMoves
 }
 
-func doPart2() int {
-	return 0
+func doPart2(crabsPerPosition []int, costs []int) int {
+	minFuel := -1
+	for targetPosition := range crabsPerPosition {
+		numFuel := getFuelPart2(crabsPerPosition, targetPosition, costs)
+		if numFuel < minFuel || minFuel < 0 {
+			minFuel = numFuel
+		}
+	}
+	return minFuel
 }
 
 func Run(path string) {
 	initialPositions := utils.ParseStringAsIntList(utils.ReadFileAsString(path), ",")
 	crabsPerPosition := getCrabsPerPosition(initialPositions)
-	fmt.Printf("input: %v\n", initialPositions)
+	costs := getCostsPart2(crabsPerPosition)
 	answer1 := doPart1(crabsPerPosition)
-	answer2 := doPart2()
+	answer2 := doPart2(crabsPerPosition, costs)
 	fmt.Printf("Part 1 answer: %v\n", answer1)
 	fmt.Printf("Part 2 answer: %v\n", answer2)
 }
