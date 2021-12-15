e202e1a9793fdf520f2556c1ba407f29319c5eef
Lâm Ngyen Hoang <lamnguyenhoang@gmail.com>
2021-12-15 13:24:25 +0100

    Day 15: part 2

--- go/days/day15/solution.go
+++ go/days/day15/solution.go
@@ -7,11 +7,10 @@ import (
 
 type locationPoint struct {
 	risk       int
-	isEnd      bool
 	x          int
 	y          int
-	key        string
 	neighbours []string
+	minToEnd   *int
 }
 
 func getKey(x, y int) string {
@@ -20,83 +19,106 @@ func getKey(x, y int) string {
 
 func createLocation(risk, x, y int) *locationPoint {
 	neighbours := make([]string, 0, 4)
+	minToEnd := -1
 	return &locationPoint{
 		risk:       risk,
-		isEnd:      false,
 		x:          x,
 		y:          y,
-		key:        getKey(x, y),
 		neighbours: neighbours,
+		minToEnd:   &minToEnd,
 	}
 }
 
-func buildLocations(locations [][]int) map[string]*locationPoint {
+func buildLocations(locations [][]int, multiplicator int) map[string]*locationPoint {
 	points := make(map[string]*locationPoint)
-	length := len(locations)
-	width := len(locations[0])
+	miniLength := len(locations)
+	miniWidth := len(locations[0])
+	length := miniLength * multiplicator
+	width := miniWidth * multiplicator
 
-	for y, row := range locations {
-		for x := range row {
-			key := getKey(x, y)
-			points[key] = createLocation(locations[y][x], x, y)
+	for i := 0; i < multiplicator; i++ {
+		for j := 0; j < multiplicator; j++ {
+			for Y, row := range locations {
+				for X := range row {
+					x := X + (miniWidth * j)
+					y := Y + (miniLength * i)
 
-			potentialNeighbours := [4][2]int{{x - 1, y}, {x + 1, y}, {x, y - 1}, {x, y + 1}}
-			for _, potentialNeighbour := range potentialNeighbours {
-				x0, y0 := potentialNeighbour[0], potentialNeighbour[1]
-				if x0 >= 0 && x0 <= width-1 && y0 >= 0 && y0 <= length-1 {
-					points[key].neighbours = append(points[key].neighbours, getKey(x0, y0))
+					risk := locations[Y][X]
+					for z := 0; z < (i + j); z++ {
+						risk = risk + 1
+						if risk == 10 {
+							risk = 1
+						}
+					}
+
+					key := getKey(x, y)
+					points[key] = createLocation(risk, x, y)
+
+					potentialNeighbours := [4][2]int{{x - 1, y}, {x + 1, y}, {x, y - 1}, {x, y + 1}}
+					for _, potentialNeighbour := range potentialNeighbours {
+						x0, y0 := potentialNeighbour[0], potentialNeighbour[1]
+						if x0 >= 0 && x0 <= width-1 && y0 >= 0 && y0 <= length-1 {
+							points[key].neighbours = append(points[key].neighbours, getKey(x0, y0))
+						}
+					}
 				}
 			}
 		}
 	}
-	points[getKey(length-1, width-1)].isEnd = true
+	minToEnd := points[getKey(length-1, width-1)].risk
+	points[getKey(length-1, width-1)].minToEnd = &minToEnd
 	return points
 }
 
-func getMinRisk(points *map[string]*locationPoint, pointKey string, riskAccumulator int, currentTrail []string, currentMin *int, currentBestTrail *[]string) {
-	point := (*points)[pointKey]
-	accumulator := riskAccumulator + point.risk
-	trail := append(currentTrail, point.key)
-
-	if point.isEnd {
-		if *currentMin < 0 || accumulator < *currentMin {
-			*currentMin = accumulator
-			currentBestTrail = &trail
+func populateMinRisk(points *map[string]*locationPoint) {
+	numPopulated := -1
+	for {
+		if numPopulated == 0 {
+			break
 		}
-		return
+		numPopulated = 0
+		for _, pointP := range *points {
+			for _, neighbourKey := range pointP.neighbours {
+				neighbourP := (*points)[neighbourKey]
+				if *neighbourP.minToEnd > 0 {
+					minToEndCandidate := pointP.risk + *neighbourP.minToEnd
+					if *pointP.minToEnd > minToEndCandidate || *pointP.minToEnd < 0 {
+						pointP.minToEnd = &minToEndCandidate
+						numPopulated++
+					}
+				}
+			}
+		}
 	}
-
-	if *currentMin > 0 && accumulator >= *currentMin {
-		return
-	}
-
-	candidates := utils.StringsNotInSlice(point.neighbours, trail)
-	if len(candidates) == 0 {
-		return
-	}
-	for _, neighbourKey := range candidates {
-		getMinRisk(points, neighbourKey, accumulator, trail, currentMin, currentBestTrail)
-	}
-
 }
 
-func doPart1(points map[string]*locationPoint) int {
+func printGrid(points map[string]*locationPoint) {
+	widthAndLength := 0
+	for _, point := range points {
+		if point.x > widthAndLength {
+			widthAndLength = point.x
+		}
+	}
+	for i := 0; i < widthAndLength; i++ {
+		for j := 0; j < widthAndLength; j++ {
+			fmt.Printf("%v ", points[getKey(j, i)].risk)
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func getMinRisk(points map[string]*locationPoint) int {
+	populateMinRisk(&points)
 	startPoint := points[getKey(0, 0)]
-	minRisk := -1
-	var bestTrail *[]string
-	getMinRisk(&points, startPoint.key, 0, make([]string, 0), &minRisk, bestTrail)
-	return minRisk - startPoint.risk
-}
-
-func doPart2() int {
-	return 0
+	return *startPoint.minToEnd - startPoint.risk
 }
 
 func Run(path string) {
 	input := utils.ReadFileAsSliceOfDigitIntSlices(path)
-	points := buildLocations(input)
-	answer1 := doPart1(points)
-	answer2 := doPart2()
+	points := buildLocations(input, 1)
+	points2 := buildLocations(input, 5)
+	answer1 := getMinRisk(points)
+	answer2 := getMinRisk(points2)
 	fmt.Printf("Part 1 answer: %v\n", answer1)
 	fmt.Printf("Part 2 answer: %v\n", answer2)
 }
