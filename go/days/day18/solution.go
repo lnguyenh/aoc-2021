aaa3de39110415d6a1a671195904a6f6919b9639
Lâm Ngyen Hoang <lamnguyenhoang@gmail.com>
2021-12-18 11:55:01 +0100

    Day 18: wip

--- go/days/day18/solution.go
+++ go/days/day18/solution.go
@@ -3,10 +3,23 @@ package day18
 import (
 	"fmt"
 	"github.com/lnguyenh/aoc-2021/utils"
+	"math"
+	"strconv"
+	"strings"
 	"unicode"
 )
 
-func needsExplode(blob []rune) int {
+func add(expression1, expression2 string) string {
+	return strings.Join([]string{
+		"[",
+		expression1,
+		",",
+		expression2,
+		"]"}, "")
+}
+
+func needsExplode(expression string) int {
+	blob := []rune(expression)
 	var stack []rune
 	for i, char := range blob {
 		if char == '[' {
@@ -21,45 +34,95 @@ func needsExplode(blob []rune) int {
 	return -1
 }
 
-func reduce(blob []rune, indexExplosion int) string {
+func getNewValue(a, b rune) string {
+	return fmt.Sprintf("%v", utils.RuneToInt(a)+utils.RuneToInt(b))
+}
+
+func insert(mainString, subString string, index int) string {
+	newString := mainString[:index] + subString + mainString[index+1:]
+	return newString
+}
+
+func explode(expression string, indexExplosion int) string {
+	blob := []rune(expression)
 	iLeft := indexExplosion + 1
 	iRight := indexExplosion + 3
 	iLeftBracket := indexExplosion
 	iRightBracket := indexExplosion + 4
+	var newLeftValue, newRightValue, leftPart, rightPart string
 
 	// left merge
+	leftPart = string(blob[:iLeftBracket])
 	for i := iLeftBracket; i >= 0; i-- {
 		if unicode.IsDigit(blob[i]) {
-			newValue := utils.DigitToRune(utils.RuneToInt(blob[i]) + utils.RuneToInt(blob[iLeft]))
-			blob[i] = newValue
+			newLeftValue = getNewValue(blob[i], blob[iLeft])
+			leftPart = insert(leftPart, newLeftValue, i)
 			break
 		}
 	}
 
 	// right merge
+	rightPart = string(blob[iRightBracket+1:])
 	for i := iRightBracket; i < len(blob); i++ {
 		if unicode.IsDigit(blob[i]) {
-			newValue := utils.DigitToRune(utils.RuneToInt(blob[i]) + utils.RuneToInt(blob[iRight]))
-			blob[i] = newValue
+			newRightValue = getNewValue(blob[i], blob[iRight])
+			insertionIndex := i - iRightBracket - 1
+			rightPart = insert(rightPart, newRightValue, insertionIndex)
 			break
 		}
 	}
 
-	leftPart := blob[:iLeftBracket]
-	rightPart := blob[iRightBracket+1:]
-	exploded := append(leftPart, '0')
-	exploded = append(exploded, rightPart...)
-	return string(exploded)
+	result := leftPart + "0" + rightPart
+	return result
 }
 
-func doPart1() int {
-	expression := "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
-	asRunes := []rune(expression)
-	fmt.Println(expression)
-	indexExplosion := needsExplode([]rune(expression))
-	reduced := reduce(asRunes, indexExplosion)
-	fmt.Printf("%v\n", reduced)
+func split(expression string) string {
+	blob := []rune(expression)
+	for i := 0; i < len(blob)-1; i++ {
+		potentialNumber, error := strconv.Atoi(string(blob[i : i+2]))
+		if error == nil {
+			up := utils.DigitToRune(int(math.Ceil(float64(potentialNumber) / 2)))
+			down := utils.DigitToRune(int(math.Floor(float64(potentialNumber) / 2.0)))
+			insert := []rune{'[', down, ',', up, ']'}
+			leftPart := blob[:i]
+			rightPart := []rune(string(blob[i+2:]))
+			processed := append(leftPart, insert...)
+			processed = append(processed, rightPart...)
+			return string(processed)
+		}
+	}
+	return string(blob)
+}
 
+func doTests() {
+	expression := "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+	fmt.Println(expression)
+	indexExplosion := needsExplode(expression)
+	exploded := explode(expression, indexExplosion)
+	fmt.Printf("%v\n", exploded)
+
+	expression = "[[[[0,7],4],[15,[0,13]]],[1,1]]"
+	fmt.Println(expression)
+	splitted := split(expression)
+	fmt.Printf("%v\n", splitted)
+}
+
+func doPart1(expressions []string) int {
+	expression := expressions[0]
+	for _, nextExpression := range expressions[1:] {
+		expression = add(expression, nextExpression)
+		for {
+			indexExplosion := needsExplode(expression)
+			if indexExplosion > 0 {
+				expression = explode(expression, indexExplosion)
+				expression = split(expression)
+				expression = split(expression)
+			} else {
+				break
+			}
+		}
+	}
+	fmt.Println(expression)
 	return 0
 }
 
@@ -70,7 +133,7 @@ func doPart2() int {
 func Run(path string) {
 	input := utils.ReadFileAsStringSlice(path, "\n")
 	fmt.Printf("input: %v\n", input)
-	answer1 := doPart1()
+	answer1 := doPart1(input)
 	answer2 := doPart2()
 	fmt.Printf("Part 1 answer: %v\n", answer1)
 	fmt.Printf("Part 2 answer: %v\n", answer2)
